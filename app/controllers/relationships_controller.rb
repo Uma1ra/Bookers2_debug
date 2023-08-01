65674441a64bdc92316a96d3e51e8491a18e8eb9
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-01 18:14:06 +0000

    [add] follow/unfollowの非同期通信化に挑戦中

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,23 +1,23 @@
 class RelationshipsController < ApplicationController
   
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
   end
   
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
   
   def follows
-    user = User.find(params[:user_id])
-    @users = user.following_users
+    @user = User.find(params[:user_id])
+    @users = @user.following_users
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.follower_users
+    @user = User.find(params[:user_id])
+    @users = @user.follower_users
   end
   
 end

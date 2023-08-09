58d339faf1a88224f1ce18d542dc5fabbd448c0b
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-09 11:49:20 +0000

    [fix] フォロ機能の非同期処理（indexとshowのプロフ側は完成）、テーブル内のフォロー数は未対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @new_book = Book.new
     @book_comment = BookComment.new
-    @user = User.find(params[:id])
+    @user = @book.user
   end
 
   def index
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,15 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
+    @users = User.all
+    # redirect_to request.referer
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
+    @users = User.all
+    # redirect_to request.referer
   end
 
   def follows
